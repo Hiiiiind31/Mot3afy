0a5fab508d9899cdee46f1b6274d11d8bad3af26
Hiiiiind_31 <hind_ahmed31@hotmail.com>
2017-10-16 15:21:56 -0700

    Welcome Activity :)

---
+++ app/src/main/java/com/mot3afy/mot3afy/Activity_Login.java
@@ -0,0 +1,13 @@
+package com.mot3afy.mot3afy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__login);
+    }
+}
---
+++ app/src/main/java/com/mot3afy/mot3afy/MyViewPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.mot3afy.mot3afy;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+/**
+ * Created by Hind on 10/16/2017.
+ */
+
+class MyViewPagerAdapter extends PagerAdapter {
+
+    private LayoutInflater layoutInflater;
+    private int[] layouts ;
+    private Context context ;
+
+
+    public MyViewPagerAdapter(int[] layouts, Context context) {
+        this.layouts = layouts;
+        this.context = context;
+
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(layouts[position], container, false);
+        container.addView(view);
+
+        return view;
+
+    }
+
+
+    @Override
+    public int getCount() {
+        return layouts.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return view == obj;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        View view = (View) object;
+        container.removeView(view);
+    }
+}
+
